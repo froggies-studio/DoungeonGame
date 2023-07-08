372a1b2f78daeaa88ce23c60ce69f1b563eafaef
valerii.korniienko <vakor.dev@gmail.com>
2023-07-08 10:18:13 +0300

    Game loose UI

---
+++ Assets/_Scripts/UI/GameLooseUI.cs
@@ -0,0 +1,31 @@
+﻿#region
+
+using UnityEngine;
+using UnityEngine.UI;
+
+#endregion
+
+namespace _Scripts.UI
+{
+    public class GameLooseUI : MonoBehaviour
+    {
+        [SerializeField] private Button restartButton;
+        [SerializeField] private Button mainMenuButton;
+
+        private void Start()
+        {
+            restartButton.onClick.AddListener(() => { });
+            mainMenuButton.onClick.AddListener(() => { });
+        }
+
+        private void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void Show()
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
