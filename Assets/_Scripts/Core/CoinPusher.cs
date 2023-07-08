3dbfff2ddcccabfdd97a11087f9d410991b0e176
valerii.korniienko <vakor.dev@gmail.com>
2023-07-08 13:45:06 +0300

    Deleted some zombie code

--- Assets/_Scripts/Core/CoinPusher.cs
+++ Assets/_Scripts/Core/CoinPusher.cs
@@ -1,36 +1,40 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+#region
+
 using UnityEngine;
-using UnityEngine.Serialization;
 
-public class CoinPusher : MonoBehaviour
+#endregion
+
+namespace _Scripts.Core
 {
-    private const int MAX_COLLIDER_COUNT = 50;
-    
-    [SerializeField] private float force;
-    [SerializeField] private float effectRadius;
-    [SerializeField] private Transform target;
-    
-    private Collider2D[] _colliders = new Collider2D[MAX_COLLIDER_COUNT];
-    
-    public void Push(Vector2 position)
+    public class CoinPusher : MonoBehaviour
     {
-        int count = Physics2D.OverlapCircleNonAlloc(position, effectRadius, _colliders);
-        for (int i = 0; i < count; i++)
-        {
-            var coinObject = _colliders[i].gameObject;
-            
-            if(!coinObject.CompareTag("Coin"))
-                continue;
-            
-            var direction = (coinObject.transform.position - target.position).normalized;
-            coinObject.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
-        }
-    }
+        private const int MAX_COLLIDER_COUNT = 50;
+        private const string COIN_TAG = "Coin";
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.DrawWireSphere(transform.position, effectRadius);
+        [SerializeField] private float force;
+        [SerializeField] private float effectRadius;
+        [SerializeField] private Transform target;
+
+        private Collider2D[] _colliders = new Collider2D[MAX_COLLIDER_COUNT];
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawWireSphere(transform.position, effectRadius);
+        }
+
+        public void Push(Vector2 position)
+        {
+            int count = Physics2D.OverlapCircleNonAlloc(position, effectRadius, _colliders);
+            for (int i = 0; i < count; i++)
+            {
+                var coinObject = _colliders[i].gameObject;
+                
+                if(!coinObject.CompareTag(COIN_TAG))
+                    continue;
+            
+                var direction = (coinObject.transform.position - target.position).normalized;
+                coinObject.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
+            }
+        }
     }
 }
--- Assets/_Scripts/UI/TrapSelectorUIComponent.cs
+++ Assets/_Scripts/UI/TrapSelectorUIComponent.cs
@@ -10,18 +10,14 @@ namespace _Scripts.UI
         [SerializeField] private CursorController cursorController;
         [SerializeField] private SelectorButtonUI trapSelectorUI;
         [SerializeField] private TrapSingleUI trapUITemplate;
-        private void Awake()
-        {
-            // trapSystem.OnTrapInfoChanged += OnTrapInfoChanged;
-        }
-
+        
         private void Start()
         {
             foreach (Transform child in transform)
             {
                 if (child != trapUITemplate.transform)
                 {
-                    Destroy(child);
+                    Destroy(child.gameObject);
                 }
             }
             
@@ -32,22 +28,6 @@ namespace _Scripts.UI
             }
             
             trapUITemplate.gameObject.SetActive(false);
-        }
-
-        private void OnTrapInfoChanged(TrapInfoSO[] obj)
-        {
-            // for (int i = 0; i < obj.Length; i++)
-            // {
-            //     var selectorObject = Instantiate(trapSelectorUI.gameObject, transform);
-            //     var selectorButtonUI = selectorObject.GetComponent<SelectorButtonUI>();
-            //     selectorButtonUI.SetTrapIndex((TrapType)(i+1), TrapSelected);
-            //     selectorButtonUI.SetTrapName(obj[i].TrapType);
-            // }
-        }
-
-        public void TrapSelected(TrapType trapType)
-        {
-            cursorController.TrapSelected(trapType);
         }
     }
 }
